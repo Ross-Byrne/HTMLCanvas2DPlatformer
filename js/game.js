1e5832ca6155e7f146d0a7d6ae024fdff4547f66
Ross Byrne <ross.byrne@outlook.ie>
2015-10-20 10:19:13 +0100

    Added some resources
    Added a coin animation that I'm gonna use later on.

--- js/game.js
+++ js/game.js
@@ -1,10 +1,14 @@
 // javascript file that holds game code
 
+// window.myNameSpace is my global object that I can add all of my functions too.
+// Setting it up, equals window(the open browser window).myNameSpace to itself if it's there or an empty object if it isn't
 window.myNameSpace = window.myNameSpace || { };
 
+// Adds GameObject object to myNameSpace, this lets me use it in main.js
 myNameSpace.GameObject = function(){
    
-   this.printStuff = function(){
+	// function to print stuff
+	this.printStuff = function(){
 
 	  console.log("This is Game!");
 
--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 // javascript file that holds the main function for the game
 
+// window.myNameSpace is my global object that I can add all of my functions too.
+// Setting it up, equals window(the open browser window).myNameSpace to itself if it's there or an empty object if it isn't
 window.myNameSpace = window.myNameSpace || { };
 
+// Adds the MainObject object to myNameSpace
 myNameSpace.MainObject = function(){
 
    // main function that runs the game
